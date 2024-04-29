cb2669a91e38c1f2350a0b360b274e0a24b32981
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2024-04-29 16:56:42 +0600

    service added

---
+++ src/Web/Home/Components/Service.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import style from "./Service.module.css"
+import Canva from "../../../assets/canva.png"
+
+import pencil from "../../../assets/pencil.png"
+import calender from "../../../assets/calander.png"
+import lightbulb from "../../../assets/lightbulb.png"
+
+const items=[{
+    title:"Sample Headline",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    img:pencil
+ },{
+    title:"Sample Headline",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    img:calender
+ },{
+    title:"Sample Headline",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    img:calender
+ },{
+    title:"Sample Headline",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    img:lightbulb
+ },{
+    title:"Sample Headline",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    img:pencil
+ },{
+    title:"Sample Headline",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    img:pencil
+ },{
+    title:"Sample Headline",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    img:lightbulb
+ },{
+    title:"Sample Headline",
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    img:calender
+ },
+ 
+ ]
+
+export default function Service() {
+  return (
+    <div className={style.service}>
+        <div className={style.service_con}>
+           <h1 className={style.h1}>
+            Why Choose us ?
+           </h1>
+           <div className={style.service_div}>
+            {items.map((item,i)=><div key={i} className={style.service_item}>
+                <img className={style.item_img} src={item.img}/>
+                <div className={style.item_div}>
+                    <h3 className={style.item_heading}><strong>{item.title}</strong></h3>
+                    <p className={style.item_desc}>{item.desc}</p>
+                </div>
+             </div>)}
+             
+           </div>
+        </div>
+    </div>
+  )
+}
--- src/Web/Home/Home.jsx
+++ src/Web/Home/Home.jsx
@@ -2,12 +2,14 @@ import style from "./Home.module.css"
 import Header from "./Components/Header"
 import Do from "./Components/Do"
 import Skill from "./Components/Skill"
+import Service from "./Components/Service"
 export default function Home() {
   return (
    <div className={style.main_home}>
       <Header/>
       <Do/>
       <Skill/>
+      <Service/>
    </div>
   )
 }
